c3f282ed620faf6e1fcf2e745a38f93b054617f5
Sunslihgt <samsam.deschamps@gmail.com>
2024-11-20 19:36:28 +0100

    Test boxes and tetris shapes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # threejs-tetris
 Small three.js Tetris game web app
+
+## To start the server
+```cmd
+npm start
+```
--- src/tetris/tetris.js
+++ src/tetris/tetris.js
@@ -1,4 +1,5 @@
 import * as threejs from 'three';
+import { SHAPES } from './shapes.js';
 
 
 const renderer = new threejs.WebGLRenderer();
@@ -8,19 +9,27 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new threejs.Scene();
 const camera = new threejs.PerspectiveCamera(
-    75,
+    35,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
-camera.position.set(1, 2, 5);
+camera.position.set(5, 10, 40);
 
-const axesHelper = new threejs.AxesHelper(5);
+const axesHelper = new threejs.AxesHelper(20);
 scene.add(axesHelper);
 
-const boxGeometry = new threejs.BoxGeometry();
-const boxMaterial = new threejs.MeshBasicMaterial({ color: 0x00FF00 });
-const box = new threejs.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+const boxesData = [
+    [0, 0, 1, 0x00FF00], // Green   -> Z
+    [0, 1, 0, 0xFF0000], // Red     -> Y
+    [1, 0, 0, 0x0000FF], // Blue    -> X
+];
+boxesData.forEach(boxData => {
+    const boxGeometry = new threejs.BoxGeometry();
+    const boxMaterial = new threejs.MeshBasicMaterial({ color: boxData[3]});
+    const box = new threejs.Mesh(boxGeometry, boxMaterial);
+    box.position.set(boxData[0], boxData[1], boxData[2]);
+    scene.add(box);
+});
 
 renderer.render(scene, camera);
